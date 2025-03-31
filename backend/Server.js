96e3323b62d598dd43dcda005e07f18989d224d0
Mariiyaa <Mariya.h.dalal.6@gmail.com>
2025-03-31 21:30:45 +0530

    31/3/

--- backend/Server.js
+++ backend/Server.js
@@ -19,6 +19,20 @@ app.use(cors({
     
   }));
 
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+    
+    next();
+  });
+  
+
 // Import routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/profile');
