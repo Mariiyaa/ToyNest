2adf8610e87cf929d5438e65fe076ae99e23ab72
Mariiyaa <Mariya.h.dalal.6@gmail.com>
2025-03-31 22:18:03 +0530

    31/3/

--- backend/Server.js
+++ backend/Server.js
@@ -45,10 +45,7 @@ const orderRoutes = require('./routes/order');
 const couponRoutes = require('./routes/coupon');
 const cartRoutes = require('./routes/cart');
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
