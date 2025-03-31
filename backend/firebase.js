049f0ed7a390993ba128784edddf0ffeb9f3fec9
Mariiyaa <Mariya.h.dalal.6@gmail.com>
2025-03-31 12:10:09 +0530

    31/3/

--- backend/firebase.js
+++ backend/firebase.js
@@ -2,6 +2,8 @@ var admin = require("firebase-admin");
 
 import serviceAccountKey from './serviceAccountKey';
 
+console.log(serviceAccountKey)
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountKey)
 });
