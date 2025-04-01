75055a75ed92350706be263971ecd7a86b903c80
Mariiyaa <Mariya.h.dalal.6@gmail.com>
2025-04-01 15:53:03 +0530

    '1/4

--- backend/Server.js
+++ backend/Server.js
@@ -8,9 +8,7 @@ const app = express();
 app.use(express.json());
 
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
@@ -34,7 +32,19 @@ app.use(cors({
     
   }));
 
+  mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT || 5000}`));
+  })
+  .catch((err) => console.error(err));
 
+mongoose.connection.on('connected', () => {
+  console.log('✅ Mongoose connected to MongoDB', process.env.MONGO_URI);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('❌ Mongoose connection error:', err);
+});
   
 
 // Import routes
